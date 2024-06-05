3b0ec7a51467a20cbcea33e4f62390437bc687bb
Maxim Baidak <m7baidak@gmail.com>
2024-06-05 17:45:29 +0300

    added prices command to file bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
-from aiogram.types import UNSET_PARSE_MODE
 from config import API_TOKEN
+from parse import fetch_crypto_prices
 
 #   Configuring logging
 logging.basicConfig(level=logging.INFO)
@@ -18,11 +18,21 @@ async def send_welcome(message: types.Message):
     await message.reply("Hi! I'm a data parsing bot.")
 
 
+# Command prices
+@dp.message(Command("prices"))
+async def get_prices(message: types.Message):
+    crypto_data = fetch_crypto_prices()
+    response_message = "\n".join(
+        [f"{item['name']}: {item['price']} (Volume: {item['volume']}, Market Cap: {item['market_cap']})" for item in
+         crypto_data])
+    await message.reply(response_message)
+
+
 # Starting bot
 async def main():
     await dp.start_polling(bot)
 
+
 if __name__ == "__main__":
     import asyncio
     asyncio.run(main())
-
